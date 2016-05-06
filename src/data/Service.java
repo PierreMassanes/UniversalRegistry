00cca57bfea4eae270332cb6346c4c67e0863219
Chloé GUGLIELMI <guglielmi.chloe@etu.unice.fr>
2016-05-06 11:05:31 +0200

    Data to example

--- src/data/Service.java
+++ src/data/Service.java
@@ -1,13 +1,14 @@
 package data;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 /**
  * Created by user on 05/05/16.
  */
-public interface Service {
-    String getInfo();
-    ReponseService accesService();
+public interface Service extends Remote {
+    String getInfo() throws RemoteException;
+    ReponseService accesService() throws RemoteException;
     void iniConnection() throws RemoteException;
     void suscribe(String clientName) throws RemoteException;
     void closeConnection() throws RemoteException;
